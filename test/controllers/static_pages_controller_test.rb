a1c00c27194ce9d77dda3bfd8e52f8dff7f1951a
Kiriyama Keisuke <keeee8800@gmail.com>
2024-02-21 04:16:49 +0900

    minitest reportersの追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def set_up
+    @base_title = "Yonda"
+  end
   test "should get top" do
     get root_path
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 end
