3866a0ba4c20f62b51b197d1a6a25b01290ff29a
Kiriyama Keisuke <keeee8800@gmail.com>
2024-02-25 21:37:00 +0900

    emailの一意性の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Keisuke Kiriyama", email: "keisuke@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   test "should be valid" do
@@ -18,5 +18,11 @@ class UserTest < ActiveSupport::TestCase
   test "email should be present" do
     @user.email = "   "
     assert_not @user.valid?
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
   end
 end
