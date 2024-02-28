ff6819a82b9e58e850b68d0ac0fd83f240e96818
Kiriyama Keisuke <keeee8800@gmail.com>
2024-02-28 13:02:50 +0900

    familyモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_27_041400) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_28_040203) do
+  create_table "families", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
