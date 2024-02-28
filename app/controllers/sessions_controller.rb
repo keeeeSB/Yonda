97f66b302ee84ae2575ec653678d2fb62300deb2
Kiriyama Keisuke <keeee8800@gmail.com>
2024-02-28 10:12:58 +0900

    remember_meチェックボックスのテスト追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       reset_session
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
       log_in user
       flash[:success] = "ログインに成功しました"
       redirect_to user
