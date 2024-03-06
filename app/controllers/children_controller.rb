701a4f8fdb1c3efca87d835fec353604b8ca4fb0
Kiriyama Keisuke <keeee8800@gmail.com>
2024-03-06 17:30:08 +0900

    お子さま登録ページの作成

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,4 +1,16 @@
 class ChildrenController < ApplicationController
   def new
+    @child = Child.new
+  end
+
+  def create
+    @child = Child.new(child_params)
+    if @child.save
+      flash[:success] = "お子さまを追加しました"
+      redirect_to @family
+    else
+      flash.now[:danger] = "お子さまを追加できませんでした"
+      render :new
+    end
   end
 end
