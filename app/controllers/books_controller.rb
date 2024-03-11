8fd10eebca0979dc18efe429c5f159b74bbf66b5
Kiriyama Keisuke <keeee8800@gmail.com>
2024-03-11 09:56:21 +0900

    searchメソッド追加、ヘッダーに検索欄を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,16 @@
 class BooksController < ApplicationController
+
+  def search
+    if params[:search].nil?
+      return
+    elsif params[:search].blank?
+      flash.now[:danger] = "検索ワードが入力されていません"
+      return
+    else
+      url = "https://www.googleapis.com/books/v1/volumes"
+      text = params[:search]
+      res = Faraday.get(url, q: text, langRestrict: 'ja', maxResults: 20)
+      @google_books = JSON.parse(res.body)
+    end
+  end
 end
