21eac5ae3d715677f055f7f849167f444ce384b1
Kiriyama Keisuke <keeee8800@gmail.com>
2024-03-11 09:59:49 +0900

    authorモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_11_005233) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_11_005907) do
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "isbn"
