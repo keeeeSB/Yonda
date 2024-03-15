2d95b84d1ce27108f3e35f92e3cdf05e689bb026
Kiriyama Keisuke <keeee8800@gmail.com>
2024-03-15 09:49:14 +0900

    リレーションを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
-  has_many :book_authors
+  has_many :book_authors, dependent: :destroy
+  has_many :authors, through: :book_authors
   has_many :read_records
   belongs_to :user
 
