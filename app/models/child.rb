de41015a40d51ddfddecf3fbeca1a99b11993b13
Kiriyama Keisuke <keeee8800@gmail.com>
2024-03-15 13:06:33 +0900

    リレーションの修正

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,4 +1,5 @@
 class Child < ApplicationRecord
   belongs_to :family
   has_many :read_records, through: :child_read_records
+  has_many :child_read_records
 end
--- app/models/read_record.rb
+++ app/models/read_record.rb
@@ -2,4 +2,5 @@ class ReadRecord < ApplicationRecord
   belongs_to :book
   belongs_to :user
   has_many :children, through: :child_read_records
+  has_many :child_read_records
 end
