1a0df480bed37c27fafcc8237ca47f534faed57c
Kiriyama Keisuke <keeee8800@gmail.com>
2024-03-20 04:29:37 +0900

    read_recordsテーブルにfamily_idを追加

--- app/models/read_record.rb
+++ app/models/read_record.rb
@@ -1,6 +1,7 @@
 class ReadRecord < ApplicationRecord
   belongs_to :book
   belongs_to :user
+  belongs_to :family
   has_many :children, through: :child_read_records
   has_many :child_read_records
 end
