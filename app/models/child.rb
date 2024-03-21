c9a350038bc97f4959aeebce0090d8998971108f
Kiriyama Keisuke <keeee8800@gmail.com>
2024-03-22 05:23:20 +0900

    childモデルにバリデーション追加

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,4 +2,8 @@ class Child < ApplicationRecord
   belongs_to :family
   has_many :read_records, through: :child_read_records
   has_many :child_read_records
+
+  validates :name, presence: true
+  validates :gender, presence: true
+  validates :birthday, presence: true
 end
