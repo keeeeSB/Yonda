95c8cdc00770ffa8b6e2197409a9985ebc74f52d
Kiriyama Keisuke <keeee8800@gmail.com>
2024-03-25 08:58:58 +0900

    軽微な修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
   has_many :read_record_tags, dependent: :destroy 
   has_many :read_records, through: :read_record_tags
-  validates :name, presences: true
+  
+  validates :name, presence: true
 end
