f1f663de2672b3cbf189afb337ec43a66c4fe1f8
Kiriyama Keisuke <keeee8800@gmail.com>
2024-03-25 12:47:16 +0900

    タグ機能の実装

--- app/controllers/read_records_controller.rb
+++ app/controllers/read_records_controller.rb
@@ -12,13 +12,14 @@ class ReadRecordsController < ApplicationController
 
   def create
     @read_record = current_user.read_records.build(read_record_params)
-    tags = params[:read_record][:name].split(',')
+    tags = params[:read_record][:tags][:name].split(',')
     if @read_record.save
       ChildReadRecord.create(child_id: params[:read_record][:child_id], read_record_id: @read_record.id)
       @read_record.save_tags(tags)
       redirect_to home_path, success: t(".success")
     else
       flash.now[:danger] = t(".failure")
+      @book = Book.find(params[:read_record][:book_id])
       render :new, status: :unprocessable_entity
     end
   end
