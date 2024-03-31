9de712ad748350a2d0bf20b596475df6084cb3d9
Kiriyama Keisuke <keeee8800@gmail.com>
2024-04-01 04:59:32 +0900

    お問い合わせページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root   "static_pages#top"
-  get    "/home",   to: "read_records#index"
-  get    "/about",  to: "static_pages#about"
-  get    "signup",  to: "users#new"
-  get    "/login",  to: "sessions#new"
-  post   "/login",  to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  get    "/home",     to: "read_records#index"
+  get    "/about",    to: "static_pages#about"
+  get    "/contact",  to: "static_pages#contact"
+  get    "signup",    to: "users#new"
+  get    "/login",    to: "sessions#new"
+  post   "/login",    to: "sessions#create"
+  delete "/logout",   to: "sessions#destroy"
   resources :users
   resources :families
   resources :children
