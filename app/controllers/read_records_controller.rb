6e63ec5bb6ef249cfb3198500cc6a8b9c978075f
Kiriyama Keisuke <keeee8800@gmail.com>
2024-04-11 10:43:11 +0900

    @book_ranksメソッドを修正

--- app/controllers/read_records_controller.rb
+++ app/controllers/read_records_controller.rb
@@ -3,7 +3,7 @@ class ReadRecordsController < ApplicationController
 
   def index
     @read_records = ReadRecord.all.order(created_at: :desc).page(params[:page])
-    @book_ranks = Book.group(:title).order('count(user_id) desc').limit(5)
+    @book_ranks = Book.group(:title, :id).order('count(user_id) desc').limit(5)
   end
 
   def new
