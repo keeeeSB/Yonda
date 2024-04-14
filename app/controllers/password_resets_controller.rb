6cee1ddb1f7b3f45ddff733ade4b575c5e41f149
Kiriyama Keisuke <keeee8800@gmail.com>
2024-04-15 04:30:20 +0900

    PasswordResetコントローラーのcreateメソッド作成

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,21 @@
 class PasswordResetsController < ApplicationController
+  
   def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:success] = t(".success")
+      redirect_to root_path
+    else
+      flash.now[:danger] = t(".failure")
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
   end
 end
