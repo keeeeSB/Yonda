293ba25bfa0f9d74f98e940ae355fe3fcfc4a3b6
Kiriyama Keisuke <keeee8800@gmail.com>
2024-04-16 05:12:50 +0900

    sorceryのインストール

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  authenticates_with_sorcery!
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
