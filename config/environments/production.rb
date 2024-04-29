a16f067c7f45c96f85dc6a476b1c9af21af8a2bb
Kiriyama Keisuke <keeee8800@gmail.com>
2024-04-29 14:16:13 +0900

    production.rbの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,8 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  host = 'Yonda.onrender.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
