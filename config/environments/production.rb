a3f6d01431381dfa922efa042dbecf5ef4abce2f
Kiriyama Keisuke <keeee8800@gmail.com>
2024-05-01 08:51:21 +0900

    production.rbの軽微な修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            ENV['SMTP_USERNAME']
-    password:             ENV['SMTP_PASSWORD']
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
