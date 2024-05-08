96258af5d7a789583cf016c7c6e8c2c9d1d60c29
Kiriyama Keisuke <keeee8800@gmail.com>
2024-05-09 00:17:21 +0900

    軽微な修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   host = 'yonda-nwgs.onrender.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { protcol: 'https', host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
