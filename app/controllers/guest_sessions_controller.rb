67df0772264311cd086459c0cf189d6083150d4b
Kiriyama Keisuke <keeee8800@gmail.com>
2024-05-14 04:21:52 +0900

    createアクションの追加

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,2 +1,11 @@
 class GuestSessionsController < ApplicationController
+
+  def create
+    user = User.find_or_create_by(email: "guest@example.com") do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
+    end
+    session[:user_id] = user.id
+    flasf[:success] = t(".success")
+  end
 end
