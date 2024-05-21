8beca2c60de2c3f7a4863ab10191b06ec11fafa8
Kiriyama Keisuke <keeee8800@gmail.com>
2024-05-22 08:25:03 +0900

    invitationsコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   resources :contacts, only: %i[new create]
   resources :account_activations, only: %i[edit]
   resources :password_resets, only: %i[new create edit update]
+  resources :invitations, only: %i[new create]
 end
