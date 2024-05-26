52cb59f72ed077bb3c5877056cab56b45f1514de
Kiriyama Keisuke <keeee8800@gmail.com>
2024-05-26 14:51:05 +0900

    invitation_mailerの修正

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,7 +1,8 @@
 class InvitationMailer < ApplicationMailer
   
-  def invite_email(email)
-    @url = new_user_url
+  def invite_email(email, inviter)
+    @url = new_family_invitation_url(family_id: inviter.family_id)
+    @inviter = inviter
     mail(to: email, subject: "Yondaアプリへの招待が届きました!")
   end
 end
