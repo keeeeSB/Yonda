94c9a8235e3e48e47c3b578fbeb41ff5f4214a86
Kiriyama Keisuke <keeee8800@gmail.com>
2024-05-27 12:35:15 +0900

    招待メール作成時にinvite_tokenを作成

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -7,7 +7,8 @@ class InvitationsController < ApplicationController
     email = params[:email]
     inviter = current_user
     if email.present?
-      InvitationMailer.invite_email(email, inviter).deliver_now
+      invite_token = SecureRandom.urlsafe_base64
+      InvitationMailer.invite_email(email, inviter, invite_token).deliver_now
       flash[:success] = "家族招待メールを送信しました"
       redirect_to home_path
     else
--- db/migrate/20240527003057_add_invitation_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddInvitationToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :invite_digest, :string
-  end
-end
