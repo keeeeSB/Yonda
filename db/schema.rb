7ed126676b8a5de75511ee688603ea26b03f0f94
Kiriyama Keisuke <keeee8800@gmail.com>
2024-05-27 10:52:31 +0900

    usersにinvite_tokenカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_27_003057) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_27_014819) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -123,7 +123,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_27_003057) do
     t.datetime "activation_sent_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
-    t.string "invite_digest"
+    t.string "invite_token"
     t.index ["email"], name: "index_users_on_email"
     t.index ["family_id"], name: "index_users_on_family_id"
   end
