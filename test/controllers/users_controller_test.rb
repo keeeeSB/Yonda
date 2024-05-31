8efff99274e0033dd86c27f7af62d6a6926aea86
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-06-01 00:05:06 +0900

    usersコントローラーのテストを追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,21 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test "新規登録ページの取得" do
     get signup_path
     assert_response :success
     assert_select "title", "新規登録 | Yonda"
+  end
+
+  def setup
+    @user = users(:tarou)
+  end
+
+  test "マイページの取得" do
+    log_in_as(@user)
+    assert_redirected_to user_path(@user)
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "title", "Tarou Exampleのページ | Yonda"
   end
 end
