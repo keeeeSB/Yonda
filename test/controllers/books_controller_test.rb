97e208cf471845624bbcf3f9156294fd4f0bb76b
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-06-04 04:32:06 +0900

    booksコントローラーのテストを作成

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "検索結果ページの取得" do
+    get search_books_path
+    assert_response :success
+    assert_select "form[action=?][method=?]", search_books_path, "get"
+  end
+
+  test "検索結果の表示" do
+    get search_books_path, params: { search: "Ruby" }
+    assert_response :success
+    assert_not_nil assigns(:google_books)
+    assert_select "div.card", minimum: 1
+  end
 end
