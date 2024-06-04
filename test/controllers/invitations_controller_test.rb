51e072c190ada2631eaa059217ba9c57cf014ad3
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-06-05 05:21:09 +0900

    invitationsコントローラーのテストを修正

--- test/controllers/invitations_controller_test.rb
+++ test/controllers/invitations_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class InvitationsControllerTest < ActionDispatch::IntegrationTest
   test "家族招待ページの取得" do
-    get invitations_new_url
+    get new_invitation_path
     assert_response :success
   end
 end
