c171d04525767a6c86ec6548fa62e7d092ac7971
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-06-05 05:14:56 +0900

    family_invitationsコントローラーのテストを修正

--- test/controllers/family_invitations_controller_test.rb
+++ test/controllers/family_invitations_controller_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 class FamilyInvitationsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:tarou)
+    @family = @user.family
+    log_in_as(@user)
+  end
+
   test "家族登録ページの取得" do
-    get family_invitations_new_url
+    get new_family_invitation_path(@family)
     assert_response :success
   end
 end
