68506ef7f0be164fa28b32e50b69f6915177b9b9
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-06-06 04:57:24 +0900

    read_recordモデルのテストを追加

--- test/models/read_record_test.rb
+++ test/models/read_record_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ReadRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @read_record = ReadRecord.new(body: "喜んでくれました!", read_date: 2024-05-19)
+  end
+
+  test "日付の必須" do
+    @read_record.read_date = nil
+    assert_not @read_record.valid?
+  end
 end
