143a336def8caa261da37250b2d277397c3609f6
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-06-06 05:30:46 +0900

    childモデルのテストを追加

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class ChildTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @child = Child.new(name: "いちろう", gender: "男の子", birthday: 2022-05-19)
+  end
+
+  test "名前の必須" do
+    @child.name = " "
+    assert_not @child.valid?
+  end
+
+  test "性別の必須" do
+    @child.gender = " "
+    assert_not @child.valid?
+  end
+
+  test "誕生日の必須" do
+    @child.birthday = nil
+    assert_not @child.valid?
+  end
 end
