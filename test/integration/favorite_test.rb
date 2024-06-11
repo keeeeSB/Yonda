2c8517fb8e922a081514be9d2eeff89b8bf82f38
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-06-11 09:03:59 +0900

    いいね機能のテストを修正

--- test/integration/favorite_test.rb
+++ test/integration/favorite_test.rb
@@ -17,4 +17,21 @@ class FavoriteTest < ActionDispatch::IntegrationTest
       post read_record_favorites_path(@read_record)
     end
   end
+
+  test "読み聞かせ記録にいいねをして、取り消す" do
+    log_in_as(@user)
+    get home_path
+    assert_response :success
+    assert_template 'read_records/index'
+
+    assert_difference '@read_record.favorites.count', 1 do
+      post read_record_favorites_path(@read_record)
+    end
+
+    @favorite = Favorite.find_by(user: @user, read_record: @read_record)
+
+    assert_difference '@read_record.favorites.count', -1 do
+      delete read_record_favorite_path(@read_record, @favorite)
+    end
+  end
 end
