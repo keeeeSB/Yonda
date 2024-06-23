5cfd5830317585c3343026ddb7f96b5579b5e1d7
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-06-24 04:54:42 +0900

    user_mailer_testの追加

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,11 +1,24 @@
 require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
-  test "パスワードリセット" do
-    mail = UserMailer.password_reset
-    assert_equal "パスワードリセット", mail.subject
-    assert_equal ["to@example.org"], mail.to
+
+  def setup
+    @user = users(:tarou)
+  end
+
+  test "アカウント認証" do
+    @user.activation_token = User.new_token
+    mail = UserMailer.account_activation(@user)
+    assert_equal "アカウント認証", mail.subject
+    assert_equal ["tarou@example.com"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+  end
+
+  test "パスワードリセット" do
+    @user.reset_token = User.new_token
+    mail = UserMailer.password_reset(@user)
+    assert_equal "パスワードリセット", mail.subject
+    assert_equal ["tarou@example.com"], mail.to
+    assert_equal ["from@example.com"], mail.from
   end
 end
